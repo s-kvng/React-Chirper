e8bbce8a2def666e301e624cba112f7e50a1fca3
nathan <kvngnathan8420@gmail.com>
2023-06-14 18:29:38 +0000

    Add delete feature

--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -43,9 +43,9 @@ class ChirpPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Chirp $chirp)
+    public function delete(User $user, Chirp $chirp): bool
     {
-        //
+        return $this->update($user , $chirp);
     }
 
     /**
