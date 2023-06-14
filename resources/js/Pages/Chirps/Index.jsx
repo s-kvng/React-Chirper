ed0a60af58aeb1e57b4b32d991b3c694e6d79d77
nathan <kvngnathan8420@gmail.com>
2023-06-14 17:16:26 +0000

    Display chirps

--- resources/js/Pages/Chirps/Index.jsx
+++ resources/js/Pages/Chirps/Index.jsx
@@ -6,18 +6,19 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 //components
 import InputError from "@/Components/InputError";
 import PrimaryButton from "@/Components/PrimaryButton";
+import Chirp from "@/Components/Chirp";
 
 //
-import { useForm, Head } from '@inertiajs/react';
+import { useForm, Head } from "@inertiajs/react";
 
-const Index = ({ auth }) => {
+const Index = ({ auth, chirps }) => {
     const { data, setData, post, processing, reset, errors } = useForm({
-        message: '',
+        message: "",
     });
 
     const submit = (e) => {
         e.preventDefault();
-        post(route('chirps.store'), { onSuccess: () => reset() });
+        post(route("chirps.store"), { onSuccess: () => reset() });
     };
 
     return (
@@ -30,15 +31,32 @@ const Index = ({ auth }) => {
                         value={data.message}
                         placeholder="What's on your mind ?"
                         className=" block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                       onChange={e => setData('message' , e.target.value)}
+                        onChange={(e) => setData("message", e.target.value)}
                     ></textarea>
                     <InputError message={errors.message} className="mt-2" />
-                    <PrimaryButton className="mt-4" disabled={processing} >Chirp</PrimaryButton>
+                    <PrimaryButton className="mt-4" disabled={processing}>
+                        Chirp
+                    </PrimaryButton>
                 </form>
+                
+
+                <div className="mt-10">
+                    {chirps.map((chirp) => (
+                       <>
+                        <div className="mt-6 bg-white rounded-lg drop-shadow-md">
+                         <Chirp key={chirp.id} chirp={chirp} />
+                         
+                       </div>
+                       <hr className="border-t border-gray-300 my-6"/>
+
+                       
+                       </>
+                       
+                    ))}
+                </div>
             </div>
         </AuthenticatedLayout>
     );
-
 };
 
 export default Index;
